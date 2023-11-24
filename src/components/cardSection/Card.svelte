67df50c94414adeb037f49ca9e1efebd4f620b5b
TeePuppet <silviu@silviu.co.uk>
2023-11-24 10:03:15 +0200

    test card size

--- src/components/cardSection/Card.svelte
+++ src/components/cardSection/Card.svelte
@@ -1,7 +1,7 @@
 <script lang="ts"></script>
 
 
-<div class="flex-none relative w-40 h-70 bg-blue-200 rounded-xl overflow-hidden"> 
+<div class="flex-none relative w-48 h-80 bg-blue-200 rounded-xl overflow-hidden"> 
     <div class="absolute mt-1 px-4 py-4">
         <h3 class="font-medium text-xl mb-2">Nume Briosa</h3>
         <p class=" text-sm leading-5">Brioșă red velvet, insertie de zmeura, crema de branza, mascarpone si frisca</p>
