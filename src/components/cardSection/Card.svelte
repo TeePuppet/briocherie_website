7445df293a5bb3c8dfffa135954a13d98325f213
TeePuppet <silviu@silviu.co.uk>
2023-11-24 10:22:03 +0200

    margin

--- src/components/cardSection/Card.svelte
+++ src/components/cardSection/Card.svelte
@@ -3,7 +3,7 @@
 
 <div class="flex-none relative w-48 h-80 bg-blue-200 rounded-xl overflow-hidden border-2 border-red-900"> 
     <div class="absolute px-4 py-3.5">
-        <h3 class="font-semibold text-xl mb-2 tracking-tight">Nume Briosa</h3>
+        <h3 class="font-semibold text-xl mb-1 tracking-tight">Nume Briosa</h3>
         <p class=" text-sm leading-5">Brioșă red velvet, insertie de zmeura, crema de branza, mascarpone si frisca</p>
     </div>
     <img alt="" class="w-full h-full object-cover" src="assets/images/image.jpg">
