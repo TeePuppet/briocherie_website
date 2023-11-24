a393072771d9b358790ed35ddf0398edc7c561db
TeePuppet <silviu@silviu.co.uk>
2023-11-24 10:20:14 +0200

    padding

--- src/components/cardSection/Card.svelte
+++ src/components/cardSection/Card.svelte
@@ -2,7 +2,7 @@
 
 
 <div class="flex-none relative w-48 h-80 bg-blue-200 rounded-xl overflow-hidden border-2 border-red-900"> 
-    <div class="absolute px-5 py-5">
+    <div class="absolute px-4 py-3.5">
         <h3 class="font-semibold text-xl mb-2">Nume Briosa</h3>
         <p class=" text-sm leading-5">Brioșă red velvet, insertie de zmeura, crema de branza, mascarpone si frisca</p>
     </div>
