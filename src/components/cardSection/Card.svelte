1febdb691b2ba5bdb9ac40d577b37c4dc0bdc8eb
TeePuppet <silviu@silviu.co.uk>
2023-11-24 10:17:56 +0200

    test card border

--- src/components/cardSection/Card.svelte
+++ src/components/cardSection/Card.svelte
@@ -1,7 +1,7 @@
 <script lang="ts"></script>
 
 
-<div class="flex-none relative w-48 h-80 bg-blue-200 rounded-xl overflow-hidden border-y-2 border-red-900 object-cover"> 
+<div class="flex-none relative w-48 h-80 bg-blue-200 rounded-xl overflow-hidden border-2 border-red-900"> 
     <div class="absolute px-5 py-5">
         <h3 class="font-semibold text-xl mb-2">Nume Briosa</h3>
         <p class=" text-sm leading-5">Brioșă red velvet, insertie de zmeura, crema de branza, mascarpone si frisca</p>
